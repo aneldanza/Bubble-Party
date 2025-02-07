4d11c74f566fe33601a357d60e865e133efa777d
Anel Danza <aneldanza23@gmail.com>
2025-02-07 16:23:04 -0500

    remove var variables and clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,53 @@
-import _ from 'lodash';
-const Game = require('./game');
-const GameView = require('./game_view');
-// export let interval;
+import _ from "lodash";
+const Game = require("./game");
+const GameView = require("./game_view");
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Initialize Firebase
-  var config = {
+  const config = {
     apiKey: "AIzaSyAEWvBjnicABetSjMV73WYidvq0M9xqssE",
     // authDomain: "bubble-party.firebaseapp.com",
     databaseURL: "https://bubble-party.firebaseio.com",
     projectId: "bubble-party",
     // storageBucket: "bubble-party.appspot.com",
-    messagingSenderId: "448048905735"
+    messagingSenderId: "448048905735",
   };
   firebase.initializeApp(config);
 
   // Get a reference to the database service
-  var database = firebase.database();
-  
+  const database = firebase.database();
+
   function highestScore(score) {
-    database.ref('/').set({
-      score: score
+    database.ref("/").set({
+      score: score,
     });
   }
 
   let previousHighScore = 0;
-  var highestScoreCount = firebase.database().ref('/');
-  highestScoreCount.on('value', function(snapshot) {
+  const highestScoreCount = firebase.database().ref("/");
+  highestScoreCount.on("value", function (snapshot) {
     previousHighScore = snapshot.val().score;
     game.highestScore = snapshot.val().score;
   });
 
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
 
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  
-  let colors = ['blue', 'yellow', 'red', 'purple', 'green', 'pink']
-  // let colors = ['blue', 'yellow'];
-  let game = new Game(colors, canvas);
-  let game_view = new GameView(game, ctx, colors, canvas);
+  const colors = ["blue", "yellow", "red", "purple", "green", "pink"];
 
-  
-    const interval = setInterval(() => {
-      if (game.over) {
-        if (game.score > previousHighScore) {
-          highestScore(game.score);
-        }
-        clearInterval(interval);
-        document.removeEventListener('mousemove', game_view.handleMouseMove);
-        game.score = 0;
-      } else if(!game_view.pause) {
-        game_view.draw()
-        
+  const game = new Game(colors, canvas);
+  const game_view = new GameView(game, ctx, colors, canvas);
+
+  const interval = setInterval(() => {
+    if (game.over) {
+      if (game.score > previousHighScore) {
+        highestScore(game.score);
       }
-    }, 5);
+      clearInterval(interval);
+      document.removeEventListener("mousemove", game_view.handleMouseMove);
+      game.score = 0;
+    } else if (!game_view.pause) {
+      game_view.draw();
+    }
+  }, 5);
 });
