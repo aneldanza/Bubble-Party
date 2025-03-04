d879333636966863b63aae9c065fa9094b4689a4
Anel Danza <aneldanza23@gmail.com>
2025-03-04 13:30:05 -0500

    remove firebase setup

--- src/index.js
+++ src/index.js
@@ -3,33 +3,6 @@ const Game = require("./game");
 const GameView = require("./game_view");
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Initialize Firebase
-  const config = {
-    apiKey: "AIzaSyAEWvBjnicABetSjMV73WYidvq0M9xqssE",
-    // authDomain: "bubble-party.firebaseapp.com",
-    databaseURL: "https://bubble-party.firebaseio.com",
-    projectId: "bubble-party",
-    // storageBucket: "bubble-party.appspot.com",
-    messagingSenderId: "448048905735",
-  };
-  firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  const database = firebase.database();
-
-  function highestScore(score) {
-    database.ref("/").set({
-      score: score,
-    });
-  }
-
-  let previousHighScore = 0;
-  const highestScoreCount = firebase.database().ref("/");
-  highestScoreCount.on("value", function (snapshot) {
-    previousHighScore = snapshot.val().score;
-    game.highestScore = snapshot.val().score;
-  });
-
   const canvas = document.getElementById("myCanvas");
   const ctx = canvas.getContext("2d");
 
@@ -40,9 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const interval = setInterval(() => {
     if (game.over) {
-      if (game.score > previousHighScore) {
-        highestScore(game.score);
-      }
       clearInterval(interval);
       document.removeEventListener("mousemove", game_view.handleMouseMove);
       game.score = 0;
